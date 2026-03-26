85f76d9dd0681b5949e4a3f88fb415e72de88ed7
jannalexa <jannalexagutierrez@gmail.com>
2026-03-26 14:16:08 +0800

    Remove config.js from tracking to protect secrets

--- js/shared/config.js
+++
@@ -1,3 +0,0 @@
-export const API_BASE_URL = "https://isodimorphous-courageously-adaline.ngrok-free.dev";
-export const WS_BASE_URL = "wss://isodimorphous-courageously-adaline.ngrok-free.dev/ws";
-export const GOOGLE_CLIENT_ID = "529435423513-68hvq21k458kqrjba7102ab32mrun948.apps.googleusercontent.com";
